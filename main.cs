0a19d8bafe3765e94773953d2ad7f547d0496d56
DisclaimedGoat <kyleturleywas@gmail.com>
2021-01-06 06:04:35 +0000

    Cleaning off the dust and some basic remodeling :P

--- main.cs
+++ main.cs
@@ -7,27 +7,6 @@ using System.IO;
 
 class MainClass {
 //Methods
-	public static string StringToBinary(string data) //Convert a string to binary
-	{
-		StringBuilder sb = new StringBuilder();
-
-		foreach (char c in data.ToCharArray())
-		{
-				sb.Append(Convert.ToString(c, 2).PadLeft(8, '0'));
-		}
-		return sb.ToString();
-	}
-
-	public static string BinaryToString(string data) //Convert binary to a string
-	{
-		List<Byte> byteList = new List<Byte>();
-
-		for (int i = 0; i < data.Length; i += 8)
-		{
-				byteList.Add(Convert.ToByte(data.Substring(i, 8), 2));
-		}
-		return Encoding.ASCII.GetString(byteList.ToArray());
-	}
 
   public static void Main (string[] args) {
 
@@ -45,26 +24,31 @@ class MainClass {
 			//Initializing the main variable that stores all of the unlockable items and their correspoding recipes
 
 		string[] combosAndRecipesArray = new [] {
-			"fire+fire", "energy", "land+land", "continent", "solarsystem+solarsystem", "galaxy",
-			"earth+earth", "dirt",	"continent+continent", "globe", "galaxy+galaxy", "*universe*",
-			"earth+energy", "earthquake", "globe+atmosphere", "planet", "sun+pressure", "*blackhole*",
-			"water+earth", "mud", "water+water", "ocean", "wind+wind", "*tornado*",
-			"water+dirt", "mud",	"dust+dust", "minerals", "clay+fire", "brick",
-			"mud+fire", "clay",	"fire+planet", "sun", "brick+brick", "wall",
-			"energy+air", "wind", "sun+cloud", "sky", "wall+wall", "home",
-			"wind+earth", "dust", "ocean+minerals", "amino acids", "home+home", "village",
-			"fire+water", "steam", "aminoacids+sun", "life", "village+village" + "city",
-			"water+air", "mist", "water+land", "river", "city+city", "metropolis",
-			"steam+air", "cloud", "water+continent", "ocean", "metropolis+metropolis", "population",
-			"fire+air", "pressure", "energy+cloud", "storm",
-			"earth+air", "dust", "storm+energy", "lightning",
-			"dirt+dirt", "land", "cloud+water", "rain",
-			"air+air", "atmosphere", "lava+water", "rock",
-			"earthquake+earth", "mountain", "wind+rock", "dust",
-			"earth+fire", "lava", "water+dust", "cloud",
-			"lava+mountain", "volcano", "planet+sun", "solar system",
-			"volcano+pressure", "eruption", "planet+planet", "solar system"
-			
+			"fire+fire", "energy", "land+land", "continent", "solarsystem+solarsystem", "galaxy", "aminoacids+energy", "life",
+			"earth+earth", "dirt",	"continent+continent", "globe", "galaxy+galaxy", "*universe*", "life+dirt", "plant",
+			"earth+energy", "earthquake", "globe+atmosphere", "planet", "sun+pressure", "*blackhole*", "plant+fire", "ash",
+			"water+earth", "mud", "water+water", "ocean", "wind+wind", "*tornado*", "ash+pressure", "coal",
+			"water+dirt", "mud",	"dust+dust", "minerals", "clay+fire", "brick", "coal+pressure", "diamond",
+			"mud+fire", "clay",	"fire+planet", "sun", "brick+brick", "wall", "plant+dirt", "tree",
+			"energy+air", "wind", "sun+cloud", "sky", "wall+wall", "home", "tree+human", "wood",
+			"wind+earth", "dust", "ocean+minerals", "aminoacids", "home+home", "village", "wood+human", "tool",
+			"fire+water", "steam", "aminoacids+sun", "life", "village+village" + "city", "rock+human", "tool",
+			"water+air", "mist", "water+land", "river", "city+city", "metropolis", "wood+wood", "home", 
+			"steam+air", "cloud", "water+continent", "ocean", "metropolis+metropolis", "population", "tool+fish", "*sushi*",
+			"fire+air", "pressure", "energy+cloud", "storm", "rock+water", "sand", "tool+bird", "*fried chicken*",
+			"earth+air", "dust", "storm+energy", "lightning", "life+land", "animal", "tool+planet", "rocket ship",
+			"dirt+dirt", "land", "cloud+water", "rain", "animal+clay", "human", "rain+rock", "dust",
+			"air+air", "atmosphere", "lava+water", "rock", "aniaml+fire", "*phoenix*", "rain+earth", "mud",
+			"earthquake+earth", "mountain", "wind+rock", "dust", "aniaml+water", "fish", "lava+air", "obsidian",
+			"earth+fire", "lava", "water+dust", "cloud", "animal+air", "bird", "diamond+wood", "diamond pickaxe",
+			"lava+mountain", "volcano", "planet+sun", "solar system", "human+human", "family", 
+			"volcano+pressure", "eruption", "planet+planet", "solar system", "family+brick", "home",
+			"diamondpickaxe+obsidian", "*nether portal*", "ocean+energy", "*hurricane*", "ocean+wind", "hurricane",
+			"continent+energy", "earthquake", "fire+wood", "fire", "lava+wood", "fire", "planet+plant", "earth","earth+rock","*moon*",
+			"river+animal", "frog", "frog+tool", "*frog legs*", "sand+sand", "beach", "beech+tree", "*palm tree*",
+			"sand+fire", "glass", "glass+glass", "glasses", "glass+earth", "*terrarium*", "glass+water", "*aquarium*",
+			"rock+tool", "metal", "metal+glass", "microscope", "microscope+life", "bacteria", "bacteria+animal", "disease", 
+			"bacteria+human", "disease", "disease+human", "death", "disease+animal", "death", "death+rock", "tomstone", 
 		};
 			//A collection of the actual recipes, but in an array to help add these objects into combosAndRecipes SortedList
 		
@@ -97,11 +81,6 @@ class MainClass {
     bool running = true;
 		bool runIntro = true;
 
-		//Variables regarding login and saving data
-		string username = "";
-		string password = "";
-		bool loggedIn = false;
-
     //Variables containing arrays of possible inputs
     string[] possExitCodes = new string[5] {"exit", "leave", "quit", "q", "e"}; 
 			//Possible input codes for quiting the game
@@ -120,9 +99,6 @@ class MainClass {
 		
 		string[] possHintCodes = new [] {"hint", "stuck", "idea"};
 			//Possible input codes for accessing the hint menu
-		
-		string[] possLoginCodes = new [] {"login", "l"};
-			//Possible input codes for accessing the login menu
 
 		//Varaibles contataing arrays of possible repsonses
 		string[] possUIRps = new string[4] 
@@ -154,7 +130,6 @@ class MainClass {
     //Main loop for the game
     while (running)
     {
-			Console.WriteLine(File.ReadAllText("test.txt"));
 			Console.WriteLine("What would you like to do? Type \"help\" if you ever need it (without the quotes).");
 			Console.Write(">> ");
       string input = Console.ReadLine().ToLower().Replace(">> ", "").Replace(" ", "");
@@ -184,12 +159,16 @@ class MainClass {
 					if (tempInven.Contains(firstIngredientInput)) {
 						hasIngredOne = true;
 					} else {
+						Console.ForegroundColor = ConsoleColor.Red;
 						Console.WriteLine("You don't have " + firstIngredientInput);
+						Console.ResetColor();
 					}
 					if (tempInven.Contains(secondIngredientInput)) {
 						hasIngredTwo = true;
 					} else {
+						Console.ForegroundColor = ConsoleColor.Red;
 						Console.WriteLine("You don't have " + secondIngredientInput);
+						Console.ResetColor();
 					}
 
 					if (hasIngredOne && hasIngredTwo) {
@@ -204,16 +183,21 @@ class MainClass {
 								trueIngredCombo = combinedIngredTwo;
 							}
 							if (inven.Contains(combosAndRecipes[trueIngredCombo])) {
+								Console.ForegroundColor = ConsoleColor.Red;
 								Console.WriteLine("You have already discovered " + combosAndRecipes[trueIngredCombo].ToUpper());
-								
+								Console.ResetColor();
 							} else {
+								Console.ForegroundColor = ConsoleColor.Green;
 								Console.WriteLine("You have discovered " + combosAndRecipes[trueIngredCombo].ToUpper() + "!");
 								inven.Add(combosAndRecipes[trueIngredCombo]);
+								Console.ResetColor();
 								Console.Write("\nPress ENTER to continue");
 								Console.ReadLine();
 							}
 						} else {
+							Console.ForegroundColor = ConsoleColor.Red;
 							Console.WriteLine("Unknown combination");
+							Console.ResetColor();
 						}
 					}
 			} else if (possInvenCodes.Contains(input))	{ //The Inventory Center
@@ -223,11 +207,11 @@ class MainClass {
 					if (inven.Count == 0) {
 							Console.WriteLine("Nothing...:\\n");
 					} else {
-
+						Console.ForegroundColor = ConsoleColor.Yellow;
 						foreach(string invenItem in inven) {
 							Console.Write("[" + invenItem.ToUpper() + "] ");
 						}
-
+						Console.ResetColor();
 					}
 					Console.WriteLine("\n\n** You have discovered " + inven.Count + "/" + (combosAndRecipes.Count + 4 - numRepeats) + " of all items **");
 					Console.Write("\nPress ENTER to continue");
@@ -244,6 +228,7 @@ class MainClass {
 				Console.WriteLine("\nINPUT\t\t\tSHORTCUT\n\ncraft\t\t\tc, crafting\ninventory\t\ti, inven, backback\nhelp\t\t\th\nshortcuts\t\ts, shorts\nhint\t\t\tstuck, idea");
 			
 			} else if (input == "clear") {
+				Console.ForegroundColor = ConsoleColor.Red;
 				Console.WriteLine("Are you sure? (Y / N)");
 				Console.Write(">> ");
       	input = Console.ReadLine().ToLower().Replace(">> ", "").Replace(" ", "");
@@ -251,7 +236,7 @@ class MainClass {
 				if (input == "y") {
 					Console.Clear();
 				}
-			
+				Console.ResetColor();
 			} else if (possHintCodes.Contains(input)) { //The Hint Center
 				Console.WriteLine("Welcome to the hint center! Whenever you get stuck, just come here!");
 				
@@ -262,24 +247,17 @@ class MainClass {
 				do {
 					hint = allPossItems[random.Next(allPossItems.Count)];
 				} while (invenArray.Contains(hint));
-
+				Console.ForegroundColor = ConsoleColor.Cyan;
 				Console.WriteLine("\nTry to get the item: " + hint.ToUpper());
+				Console.ResetColor();
 
 				Console.Write("\nPress ENTER to continue");
 				Console.ReadLine();
-			} else if (possLoginCodes.Contains(input)) {
-				Console.WriteLine("Login to save and load your progress:");
-				Console.Write("USERNAME: ");
-				username = Console.ReadLine().ToLower().Replace("USERNAME: ", "").Replace(" ", "");
-				Console.Write("PASSWORD: ");
-				password = Console.ReadLine().ToLower().Replace("PASSWORD: ", "").Replace(" ", "");
-
-				
-
 			} else {
+				Console.ForegroundColor = ConsoleColor.Red;
 				int randInt = random.Next(possUIRps.Length);
 				Console.WriteLine(possUIRps[randInt]);
-
+				Console.ResetColor();
 			}
 			Console.Write("\n");
     }
